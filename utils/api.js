298556b456b2a0348ae4420e6926b71192f7c1d9
fanburger <yinfanburger@gmail.com>
2023-04-20 23:04:21 +0800

    complete me page

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,7 @@
 const request = require('./request')
-const {getUserIDFromStorage} = require('./util')
+const {
+  getUserIDFromStorage
+} = require('./util')
 
 function login(data) {
   return request.get('/login', data)
@@ -11,12 +13,27 @@ function verify() {
 
 
 function creatBook(data) {
-  const userID= getUserIDFromStorage()
-  return request.post(`/book/create?user_id=${userID}`,data)
+  const userID = getUserIDFromStorage()
+  return request.post(`/book/create?user_id=${userID}`, data)
+}
+
+function getBookListCreatedByUser(data) {
+  const userID = getUserIDFromStorage()
+  data['user_id'] = userID
+  return request.get('/book/list/user', data)
+}
+
+function getUserSummary() {
+  const userID = getUserIDFromStorage()
+  const data = {}
+  data['user_id'] = userID
+  return request.get('/users/summary', data)
 }
 
 module.exports = {
   login,
   verify,
-  creatBook
+  creatBook,
+  getBookListCreatedByUser,
+  getUserSummary
 }
