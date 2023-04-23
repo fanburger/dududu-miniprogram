5a7f86b728e54191fecf3a2f347add67733e3edf
fanburger <yinfanburger@gmail.com>
2023-04-23 15:08:13 +0800

    add avatars cloud path

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const hostUrl = 'http://127.0.0.1:5000'
-const defaultAvatar = 'cloud://dudududu-6ggzozxbab973851.6475-dudududu-6ggzozxbab973851-1317867746/default_avatar.png	'
+const avatars = 'cloud://dudududu-6ggzozxbab973851.6475-dudududu-6ggzozxbab973851-1317867746/avatars'
 module.exports = {
   hostUrl,
-  defaultAvatar
+  avatars
 }
