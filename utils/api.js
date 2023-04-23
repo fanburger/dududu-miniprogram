eb9cdba20aacf17a6b708d9360cc846f9b6c8c84
fanburger <yinfanburger@gmail.com>
2023-04-23 15:06:45 +0800

    add api

--- utils/api.js
+++ utils/api.js
@@ -11,7 +11,6 @@ function verify() {
   return request.get('/verify')
 }
 
-
 function creatBook(data) {
   const userID = getUserIDFromStorage()
   return request.post(`/book/create?user_id=${userID}`, data)
@@ -25,6 +24,26 @@ function getBookListCreatedByUser(data) {
 
 function getBookDetail(data) {
   return request.get('/book/detail', data)
+}
+
+function getBookList(data) {
+  return request.get('/book/list', data)
+}
+
+function bookJudge(data) {
+  const userID = getUserIDFromStorage()
+  data['user_id'] = userID
+  return request.post(`/book/judge?user_id=${userID}`, data)
+}
+
+function getBookUsers(data) {
+  const userID = getUserIDFromStorage()
+  data['user_id'] = userID
+  return request.get('/book/users', data)
+}
+
+function bookBatchList(data) {
+  return request.post('/book/batch_list', data)
 }
 
 function getUserSummary() {
@@ -46,13 +65,31 @@ function createArticle(data) {
   return request.post(`/comment/create/article?user_id=${userID}`, data)
 }
 
+function getCommentListArticle() {
+  const data = {
+    page: 1,
+    size: 5
+  }
+  return request.get('/comment/list/artical', data)
+}
+
+function usersBatchMe(users) {
+  return request.post('/users/batch_me', users)
+}
+
 module.exports = {
   login,
   verify,
   creatBook,
   createArticle,
+  getCommentListArticle,
   getBookDetail,
+  getBookList,
   getBookListCreatedByUser,
+  bookJudge,
+  getBookUsers,
+  bookBatchList,
   getUserSummary,
-  getMe
+  getMe,
+  usersBatchMe
 }
